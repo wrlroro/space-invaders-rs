1b07ba06ec9166abbca6eb708f9918b10032f562
wrlroro <torres.robertj@icloud.com>
2025-08-09 19:17:07 +0200

    Aliens have health and can die

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,41 @@ use std::time::Duration;
 
 const PIXEL: u32 = 10;
 
+#[derive(Clone)]
+struct Alien {
+    sprite: Vec<Vec<i32>>,
+    x: i32,
+    y: i32,
+    current_health: i32,
+    alive: bool,
+}
+
+impl Alien {
+    fn new(sprite: Vec<Vec<i32>>, x: i32, y: i32, current_health: i32) -> Self {
+        Self {
+            sprite,
+            x,
+            y,
+            current_health,
+            alive: true,
+        }
+    }
+
+    fn take_damage(&mut self, damage: i32) {
+        if !self.alive { return; }
+        self.current_health = (self.current_health - damage).max(0);
+        if self.current_health == 0 {
+            self.alive = false;
+        }
+    }
+     
+    fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+        if !self.alive { return; }
+        drawing(canvas, &self.sprite, self.x, self.y)
+    }
+
+}
+
 fn drawing(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, design: &Vec<Vec<i32>>, x: i32, y: i32) {
     canvas.set_draw_color(Color::WHITE);
 
@@ -60,8 +95,19 @@ pub fn main() {
 
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
-    
+
     let mut pos_x = 100;
+
+    let mut aliens = vec![
+        Alien::new(alien_1.clone(), 100, 200, 3),
+        Alien::new(alien_1.clone(), 200, 200, 3),
+        Alien::new(alien_1.clone(), 300, 200, 3),
+        Alien::new(alien_1.clone(), 400, 200, 3),
+        Alien::new(alien_1.clone(), 500, 200, 3),
+        Alien::new(alien_2.clone(), 150, 100, 5),
+        Alien::new(alien_2.clone(), 300, 100, 5),
+        Alien::new(alien_2.clone(), 450, 100, 5),
+    ];
 
     let mut i = 0;
     'running: loop {
@@ -70,8 +116,10 @@ pub fn main() {
         canvas.clear();
 
         drawing(&mut canvas, &spaceship, pos_x, 550);
-        drawing(&mut canvas, &alien_1, 400, 100); 
-        drawing(&mut canvas, &alien_2, 200, 100);
+
+        for alien in &aliens {
+            alien.draw(&mut canvas);
+        }
 
         for event in event_pump.poll_iter() {
             match event {
@@ -89,6 +137,13 @@ pub fn main() {
                     pos_x += 10;
                     pos_x = pos_x.clamp(0, 770);
                 },
+
+                // Testing if health works
+                Event::KeyDown { keycode: Some(Keycode::Space), .. } => {
+                    for alien in aliens.iter_mut() {
+                        alien.take_damage(1);
+                    }
+                }
 
                 _ => {}
             }
